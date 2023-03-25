62ee2a53054c00f6b9073f3cffb8538dc8b53d9e
sjk <sungntung@gmail.com>
2023-03-25 20:15:11 +0900

    change port to 3001

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,6 @@ We're planning to expand the API in the future!
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
