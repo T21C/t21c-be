132f018eff03d01f1a752f04c58d2ee6cbdda9a5
sjk <sungntung@gmail.com>
2023-08-19 19:26:24 +0900

    gsheets: remove query

--- src/gsheets/gsheets.service.ts
+++ src/gsheets/gsheets.service.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export class GsheetsService {
   async getDataFromSheets() {
     const response: any = await axios.get(
-      'https://spreadsheets.google.com/spreadsheets/d/1eaA1gyZ-6OWFthHFcVTfLV62U_MbpP6PHc8udN24iCg/gviz/tq?sheet=Levels%20by%20ID&tq=SELECT%20*',
+      'https://spreadsheets.google.com/spreadsheets/d/1eaA1gyZ-6OWFthHFcVTfLV62U_MbpP6PHc8udN24iCg/gviz/tq?sheet=Levels%20by%20ID',
     );
     const gvizStr: string = response.data;
 
