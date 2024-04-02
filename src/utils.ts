bafeb4293670d81b1400b7b584d82312bc05715e
sjk <sungntung@gmail.com>
2024-04-02 22:16:02 +0900

    fix(pass): fix a lot of bugs

--- src/utils.ts
+++ src/utils.ts
@@ -111,56 +111,62 @@ export function calculatePP(
     } else if (speed > 1) {
       speedMtp = 2 - speed;
     }
-  } else if (speed == 1) {
-    speedMtp = 1;
-  } else if (speed < 1) {
-    speedMtp = 0;
-  } else if (speed < 1.1) {
-    speedMtp = 25 * (speed - 1.1) ** 2 + 0.75;
-  } else if (speed < 1.2) {
-    speedMtp = 0.75;
-  } else if (speed < 1.25) {
-    speedMtp = 50 * (speed - 1.2) ** 2 + 0.75;
-  } else if (speed < 1.3) {
-    speedMtp = -50 * (speed - 1.3) ** 2 + 1;
-  } else if (speed < 1.5) {
-    speedMtp = 1;
-  } else if (speed < 1.75) {
-    speedMtp = 2 * (speed - 1.5) ** 2 + 1;
-  } else if (speed < 2) {
-    speedMtp = -2 * (speed - 2) ** 2 + 1.25;
   } else {
-    speedMtp = 0;
+    if (speed == 1) {
+      speedMtp = 1;
+    } else if (speed < 1) {
+      speedMtp = 0;
+    } else if (speed < 1.1) {
+      speedMtp = 25 * (speed - 1.1) ** 2 + 0.75;
+    } else if (speed < 1.2) {
+      speedMtp = 0.75;
+    } else if (speed < 1.25) {
+      speedMtp = 50 * (speed - 1.2) ** 2 + 0.75;
+    } else if (speed < 1.3) {
+      speedMtp = -50 * (speed - 1.3) ** 2 + 1;
+    } else if (speed < 1.5) {
+      speedMtp = 1;
+    } else if (speed < 1.75) {
+      speedMtp = 2 * (speed - 1.5) ** 2 + 1;
+    } else if (speed < 2) {
+      speedMtp = -2 * (speed - 2) ** 2 + 1.25;
+    } else {
+      speedMtp = 0;
+    }
   }
 
   //get scorev2 multiplier
   if (misses == 0) {
     scorev2Mtp = 1.1;
-  }
-
-  let tp = (start + end) / 2;
-  let tpDeduc = (startDeduc + endDeduc) / 2;
-  let am = Math.max(0, misses - Math.floor(tileCount / gmConst));
-
-  if (am <= 0) {
-    scorev2Mtp = 1;
-  } else if (am <= start) {
-    scorev2Mtp = 1 - startDeduc / 100;
-  } else if (am <= tp) {
-    let kOne =
-      (Math.pow((am - start) / (tp - start), pwr) * (tpDeduc - startDeduc)) /
-      100;
-    scorev2Mtp = 1 - startDeduc / 100 - kOne;
-  } else if (am <= end) {
-    const kTwo =
-      (Math.pow((end - am) / (end - tp), pwr) * (endDeduc - tpDeduc)) / 100;
-    scorev2Mtp = 1 - kTwo - endDeduc / 100;
   } else {
-    scorev2Mtp = 1 - endDeduc / 100;
+    let tp = (start + end) / 2;
+    let tpDeduc = (startDeduc + endDeduc) / 2;
+    let am = Math.max(0, misses - Math.floor(tileCount / gmConst));
+
+    if (am <= 0) {
+      scorev2Mtp = 1;
+    } else if (am <= start) {
+      scorev2Mtp = 1 - startDeduc / 100;
+    } else if (am <= tp) {
+      let kOne =
+        (Math.pow((am - start) / (tp - start), pwr) * (tpDeduc - startDeduc)) /
+        100;
+      scorev2Mtp = 1 - startDeduc / 100 - kOne;
+    } else if (am <= end) {
+      const kTwo =
+        (Math.pow((end - am) / (end - tp), pwr) * (endDeduc - tpDeduc)) / 100;
+      scorev2Mtp = 1 + kTwo - endDeduc / 100;
+    } else {
+      scorev2Mtp = 1 - endDeduc / 100;
+    }
   }
 
   //get score
-  score = Math.max(1, baseScore * xaccMtp * speedMtp);
+  if (isDesertBus) {
+    score = Math.max(1, baseScore * xaccMtp * speedMtp);
+  } else {
+    score = baseScore * xaccMtp * speedMtp;
+  }
 
   //get scorev2
   if (isNoHoldTap) {
