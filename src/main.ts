f9f70c37a5cb110b717ca926fb648a393352c350
sjk <sungntung@gmail.com>
2024-05-02 00:35:49 +0000

    chore: compression, update description

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,21 +13,23 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   });
+  app.use(compression());
 
   const config = new DocumentBuilder()
-    .setTitle('T21+C API')
+    .setTitle('TUF API')
     .setDescription(
-      `This is an API for the 21+ Collective, specifically the 21 Forums!
+      `This is the official open API for The Universal Forums.
 
 You can currently:
 
 - view level info
+- view passes info
   
 We're planning to expand the API in the future!
 `,
     )
     .setExternalDoc(
-      'The 21 Forums',
+      'The Universal Forums',
       'https://docs.google.com/spreadsheets/d/1eaA1gyZ-6OWFthHFcVTfLV62U_MbpP6PHc8udN24iCg/edit',
     )
     .setVersion('0.0.1')
